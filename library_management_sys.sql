654483b2ff20a4cfe76bdafc70b0c7844e0465cc
pr0jectf3 <portillozachary@gmail.com>
2019-12-01 05:42:11 -0800

    finished project

--- library_management_sys.sql
+++ library_management_sys.sql
@@ -4,8 +4,8 @@ CREATE DATABASE Library;
 USE Library;
 
 CREATE TABLE Loan_Type(
-	Type varchar(20),
-    Category varchar(20),
+	Type varchar(50),
+    Category varchar(50),
     Max_Loaned int,
     Loan_period int,
     Extension int,
@@ -18,7 +18,7 @@ CREATE TABLE Borrower(
     Minit char,
     Lname varchar(20),
     CardID char(7),
-    Btype varchar(20),
+    Btype varchar(50),
     Department varchar(100),
     Email varchar(50),
     Sex char(1),
@@ -123,7 +123,7 @@ BEGIN
     Select Btype Into @type From Borrower Where CardID In (Select CardID From Book_Loans Where LoanID = Loan);
     Select Date_Expected Into @expected From Book_Loans Where LoanID = Loan;
     Select Late_Fine,Loan_Period,Extension Into @fine,@period,@renewperiod From Loan_Type Where Type = @type And Category = @category;
-    
+
     Set @newExpected = Date_add(@expected, Interval @renewperiod Day);
     Set @daysSinceExpected = datediff(CURDATE(),@newExpected);
     Set @fee = 0.00;
@@ -186,21 +186,25 @@ CREATE TRIGGER `check_before_returning_and_renewal` BEFORE UPDATE
 ON `Book_Loans`
 FOR EACH ROW
 BEGIN
-	Select datediff(CURDATE(),OLD.Date_Expected) Into @datediff From Book_Loans Where CardID = OLD.CardID;
-     
+	Select datediff(CURDATE(),OLD.Date_Expected) Into @datediff From Book_Loans Where LoanID = OLD.LoanID;
+	Select Category Into @category From Books Where BookID In(Select BookID From Book_Copies Where CopyID In(Select CopyID From Book_Loans Where LoanID = NEW.LoanID));
     
-    If @datediff > 0 Then
-		Signal sqlstate "45000" SET message_text = "RENEWAL REJECTED: BORROWER HAS AN OVERDUE BOOK";
-	END IF;
+     If @datediff > 0 And NEW.Date_Returned Is Null Then
+ 		Signal sqlstate "45000" SET message_text = "RENEWAL REJECTED: BORROWER HAS AN OVERDUE BOOK";
+ 	END IF;
 	If OLD.Extensions_Taken > 1 Then
 		Signal sqlstate "45000" SET message_text = "RENEWAL REJECTED: BORROWER HAS REACHED THEIR MAXIMUM RENEWAL ON THIS LOAN";
 	END IF;
+    If @category = "New" And NEW.Date_Returned Is Null Then
+ 		Signal sqlstate "45000" SET message_text = "RENEWAL REJECTED: BORROWER CANNOT RENEW A NEW BOOK";
+ 	END IF;
     If OLD.BranchID != NEW.BranchID Then
 		Signal sqlstate "45000" SET message_text = "RETURN REJECTED: BORROWER HAS ATTEMPTED TO RETURN AT THE WRONG BRANCH";
 	END IF;
     If OLD.Date_Loaned = NEW.Date_Returned Then
 		Signal sqlstate "45000" SET message_text = "RETURN REJECTED: BORROWER CANNOT RETURN A BOOK THE SAME DAY IT WAS BORROWED";
 	END IF;
+    
 END;//
 DELIMITER ;
 
@@ -230,7 +234,19 @@ CREATE TRIGGER `check_before_borrowing` Before Insert
 ON `Book_Loans`
 FOR EACH ROW
 BEGIN
-	
+	Select Sum(Fee) Into @fee From Book_Loans Where CardID = NEW.CardID;
+    Select Btype Into @type From Borrower Where CardID = NEW.CardID;
+    Select Category Into @category From Books Where BookID In(Select BookID From Book_Copies Where CopyID = NEW.CopyID);
+    Select Max_Loaned Into @max From Loan_Type Where Type = @type And Category = @category;
+    Select Count(CopyID) Into @count From Book_Loans Where CopyID In
+ 		(Select CopyID From Book_Copies Where BookID In(Select BookID From Books Where Category = @category)) And CardID = NEW.CardID;
+        
+    If @fee > 0.00 Then
+		Signal sqlstate "45000" SET message_text = "LOAN REJECTED: BORROWER HAS FEES THAT ARE UNPAID";
+	End If;
+    If 	@count > @max Then
+		Signal sqlstate "45000" SET message_text = "LOAN REJECTED: BORROWER HAS REACHED MAXIMUM LOANS IN THAT BOOK CATEGORY";
+	End If;
 END;//
 DELIMITER ;
 
@@ -238,33 +254,33 @@ INSERT INTO Loan_Type(Type,Category,Max_Loaned,Loan_Period,Extension,Late_Fine)
 Values
 	("Faculty","English",12,90,30,0.2),
 	("Faculty","Foreign",3,60,30,0.5),
-	("Faculty","New",1,7,NULL,0.5),
+	("Faculty","New",1,7,0,0.5),
 	("Staff","English",7,60,30,0.2),
 	("Staff","Foreign",1,30,30,0.5),
-	("Staff","New",1,7,NULL,0.5),
-	("Graduate","English",12,60,30,0.2),
-	("Graduate","Foreign",3,60,30,0.5),
-	("Graduate","New",1,7,NULL,0.5),
-	("Undergraduate","English",8,60,30,0.2),
-	("Undergraduate","Foreign",2,60,30,0.5),
-	("Undergraduate","New",1,7,NULL,0.5),
-	("Vocational","English",5,60,15,0.2),
-	("Vocational","Foreign",1,30,15,0.5),
-	("Vocational","New",1,7,NULL,0.5);
+	("Staff","New",1,7,0,0.5),
+	("Graduate Student","English",12,60,30,0.2),
+	("Graduate Student","Foreign",3,60,30,0.5),
+	("Graduate Student","New",1,7,0,0.5),
+	("Undergraduate Student","English",8,60,30,0.2),
+	("Undergraduate Student","Foreign",2,60,30,0.5),
+	("Undergraduate Student","New",1,7,0,0.5),
+	("Vocational Student","English",5,60,15,0.2),
+	("Vocational Student","Foreign",1,30,15,0.5),
+	("Vocational Student","New",1,7,0,0.5);
     
 INSERT INTO Borrower(Fname,Minit,Lname,CardID,Btype,Department,Email,Sex,Bdate,Phone)
 Values
-	("John","L","Smith","1111111","Faculty","Engineering","jls@gmail.com","M","1965-12-08","3109873425"),
-	("Sam","R","Westner","2222222","Faculty","English","srw@gmail.com","M","1998-01-01","1896325874"),
-	("Zachary","J","Perry","3333333","Faculty","English","zjp@gmail.com","M","1987-02-05","3946751248"),
-	("Oscar","M","Polonco","4444444","Faculty","Health","omp@gmail.com","M","1985-03-09","1234996525"),
+	("John","L","Smith","1111111","Faculty","Electrical Engineering","jls@gmail.com","M","1965-12-08","3109873425"),
+	("Sam","R","Westner","2222222","Faculty","Electrical Engineering","srw@gmail.com","M","1998-01-01","1896325874"),
+	("Zachary","J","Perry","3333333","Vocational Student","Electrical Engineering","zjp@gmail.com","M","1987-02-05","3946751248"),
+	("Oscar","M","Polonco","4444444","Vocational Student","Electrical Engineering","omp@gmail.com","M","1985-03-09","1234996525"),
 	("Joshua","W","Talter","5555555","Staff","Business","jwt@gmail.com","M","1995-04-10","3325652548"),
-	("Rebecca","C","Taylor","9999999","Faculty","Engineering","rct@gmail.com","F","1996-07-17","1325698569"),
-	("Maddeline","T","Black","1234567","Faculty","Business","mtb@gmail.com","F","1996-08-19","3365289658"),
-	("Darline","J","Dasini","7654321","Faculty","Health","djd@gmail.com","F","1997-08-23","1313125632"),
-	("Jessica","L","Facht","0000000","Faculty","Engineering","jlf@gmail.com","F","1995-09-05","7878541259"),
-	("Joana","Q","Garcia","1212121","Staff","English","jqg@gmail.com","F","1989-09-14","3696369636"),
-	("Brenda","E","Wo","4545454","Staff","Engineering","bew@gmail.com","F","1988-11-02","4565456321");
+	("Rebecca","C","Taylor","9999999","Undergraduate Student","Computer Science","rct@gmail.com","F","1996-07-17","1325698569"),
+	("Maddeline","T","Black","1234567","Undergraduate Student","Computer Science","mtb@gmail.com","F","1996-08-19","3365289658"),
+	("Darline","J","Dasini","7654321","Graduate Student","Computer Science","djd@gmail.com","F","1997-08-23","1313125632"),
+	("Jessica","L","Facht","0000000","Graduate Student","Finance","jlf@gmail.com","F","1995-09-05","7878541259"),
+	("Joana","Q","Garcia","1212121","Staff","Finance","jqg@gmail.com","F","1989-09-14","3696369636"),
+	("Brenda","E","Wo","4545454","Staff","Finance","bew@gmail.com","F","1988-11-02","4565456321");
 
 INSERT INTO Branch(BranchID,Bname)
 Values
@@ -302,6 +318,12 @@ Values
     ("1234","789456",1,"Available"),
     ("5678","111122",2,"Available"),
     ("1010","999636",3,"Available"),
+    ("3243","185526",1,"Available"),
+    ("1058","123456",3,"Available"),
+    ("3526","123456",3,"Available"),
+    ("1946","785825",1,"Avaiable"),
+    ("2534","785825",3,"Available"),
+    ("0890","000258",2,"Available"),
     ("1212","999636",1,"Available");
 
 INSERT INTO Book_Authors(BookID,Author_Name)
@@ -316,26 +338,33 @@ Values
 	("000258","Gale Lackman"),
 	("789456","Alexandria Cortez");
 
-Call Borrow("123","1111111","1111","2018-11-27");
-Call Borrow("456","2222222","7777","2019-10-05");
-Call Borrow("789","3333333","3333","2019-01-23");
-Call Borrow("222","3333333","4444","2017-05-01");
-Call Borrow("111","3333333","2222","2019-02-10");
-Call Borrow("444","0000000","1234","2019-06-11");
-Call Borrow("999","9999999","1010","2019-10-18");
+Call Borrow("123","1111111","1111","2019-06-14");
+Call Borrow("456","2222222","7777","2019-10-23");
+Call Borrow("789","3333333","3333","2019-11-25");
+Call Borrow("222","3333333","4444","2019-11-25");
+Call Borrow("111","3333333","2222","2019-08-24");
+Call Borrow("444","0000000","1234","2019-10-17");
+Call Borrow("999","9999999","1010","2019-06-18");
+Call Borrow("185","7654321","9999","2019-10-27");
+Call Borrow("264","1212121","3243","2019-11-15");
+Call Borrow("958","5555555","1946","2019-11-11");
+Call Borrow("134","4545454","1058","2019-07-01");
 
 
-Call Renewal("999");
--- -- Call Renewal("222");
+Call Renewal("958");
+Call Renewal("444");
+Call Renewal("185");
+Call Renewal("456");
 
--- Call ReturnBook("222",1,"2017-05-23");
--- Select * From Book_Copies;
--- Call ReturnBook("999",3,"2019-11-01");
+Call ReturnBook("222",1,"2019-11-20");
+Call ReturnBook("444",1,"2019-10-25");
+Call ReturnBook("789",3,"2019-11-24");
+Call ReturnBook("134",3,"2019-10-23");
+Call ReturnBook("123",1,"2019-11-16");
 
--- Call Borrow("221","3333333","4444","2019-11-28");
-Select * From Book_Loans;
+Select * From Borrower;
 
--- Select Loan_period From Loan_Type Where Type = 'Faculty' And Category = 'English';
+
 
 
 
